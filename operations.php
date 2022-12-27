ef26abe30ea38153f09eac798a7c1db67beffeca
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-12-27 18:48:49 +0530

    operations

---
+++ operations.php
@@ -0,0 +1,14 @@
+<?php 
+    //adding numbers in quotes
+    $x = 5 + 5;
+    $y = '5' + 5;
+    $z = '5' + '5';
+    var_dump($x,$y,$z);
+
+    //operations
+    echo 10 - 8;
+    echo 4 * 3;
+    echo 8 / 3;
+    echo 6 % 4;
+
+?>
