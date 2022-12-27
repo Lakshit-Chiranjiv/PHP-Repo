fc4d00219806d0896580a1b891073bcb7450d169
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-12-27 18:39:12 +0530

    basic datatypes

---
+++ variables_datatypes.php
@@ -0,0 +1,19 @@
+<?php 
+    //datatypes & variables
+    //string
+    $name = 'Lakshit';
+
+    //integer
+    $age = 21;
+
+    //boolean
+    $is_best = true;
+
+    //float
+    $efficiency = 99.9;
+
+    echo $name,$age,$is_best,$efficiency;
+    var_dump($is_best);
+    
+
+?>
