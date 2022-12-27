b8aca917e3bbca944c74f7e689facda4fdf8b184
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-12-27 18:22:21 +0530

    in html php render

--- outputs.php
+++ outputs.php
@@ -21,3 +21,18 @@
     var_export([3,29,9]);
     var_export(42);
 ?>
+
+<!-- in html render -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo 'Heading 1'; ?></h1>
+    <h2><?= 'heading 2' ?></h2>
+</body>
+</html>
