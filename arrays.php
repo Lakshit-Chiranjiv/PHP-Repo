1ba4f439be8c3483573dd7339f6ad02fbd080799
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-12-29 00:32:28 +0530

    array basics

---
+++ arrays.php
@@ -0,0 +1,13 @@
+<?php 
+    //arrays basics
+    $nums = [5,2,7,2,4];
+
+    print_r($nums);
+    var_dump($nums);
+
+    $names = array('messi','neymar','ronaldo');
+    print_r($names);
+    var_dump($names);
+
+
+?>
